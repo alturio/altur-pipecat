105b02ca6c5e2483aeb36c330a967a18f3deecdf
Pedro Fernandez <pedroandresfdz@hotmail.com>
2025-02-04 22:37:28 +0000

    update altur frame serializer

--- src/pipecat/serializers/altur.py
+++ src/pipecat/serializers/altur.py
@@ -5,7 +5,7 @@ import json
 
 from pydantic import BaseModel
 
-from pipecat.audio.utils import pcm_to_ulaw, ulaw_to_pcm
+from pipecat.audio.utils import create_default_resampler, pcm_to_ulaw, ulaw_to_pcm
 from pipecat.frames.frames import (
     AudioRawFrame,
     Frame,
@@ -13,6 +13,8 @@ from pipecat.frames.frames import (
     InputDTMFFrame,
     KeypadEntry,
     StartInterruptionFrame,
+    TransportMessageFrame,
+    TransportMessageUrgentFrame,
 )
 from pipecat.serializers.base_serializer import FrameSerializer, FrameSerializerType
 
@@ -31,17 +33,24 @@ class AlturFrameSerializer(FrameSerializer):
         self._call_id = call_id
         self._params = params
 
+        self._resampler = create_default_resampler()
+
     @property
     def type(self) -> FrameSerializerType:
         """Return the type of the AlturFrameSerializer."""
         return FrameSerializerType.TEXT
 
-    def serialize(self, frame: Frame) -> str | bytes | None:
+    async def serialize(self, frame: Frame) -> str | bytes | None:
         """Serialize a frame to a string."""
+        if isinstance(frame, StartInterruptionFrame):
+            # TODO: Implement this
+            pass
         if isinstance(frame, AudioRawFrame):
             data = frame.audio
 
-            serialized_data = pcm_to_ulaw(data, frame.sample_rate, self._params.altur_sample_rate)
+            serialized_data = await pcm_to_ulaw(
+                data, frame.sample_rate, self._params.altur_sample_rate, self._resampler
+            )
             payload = base64.b64encode(serialized_data).decode("utf-8")
             response = {
                 "call_id": self._call_id,
@@ -49,12 +58,10 @@ class AlturFrameSerializer(FrameSerializer):
             }
 
             return json.dumps(response)
+        elif isinstance(frame, (TransportMessageFrame, TransportMessageUrgentFrame)):
+            return json.dumps(frame.message)
 
-        if isinstance(frame, StartInterruptionFrame):
-            # TODO: Implement this
-            pass
-
-    def deserialize(self, data: str | bytes) -> Frame | None:
+    async def deserialize(self, data: str | bytes) -> Frame | None:
         """Deserialize a string to a frame."""
         payload = json.loads(data)
 
@@ -62,8 +69,8 @@ class AlturFrameSerializer(FrameSerializer):
             return None
 
         payload = base64.b64decode(payload["payload"])
-        deserialized_data = ulaw_to_pcm(
-            payload, self._params.altur_sample_rate, self._params.sample_rate
+        deserialized_data = await ulaw_to_pcm(
+            payload, self._params.altur_sample_rate, self._params.sample_rate, self._resampler
         )
         audio_frame = InputAudioRawFrame(
             audio=deserialized_data, num_channels=1, sample_rate=self._params.sample_rate
